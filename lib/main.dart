3ae96ae98e6d223c3f5d41119303a0d8297f405e
Willian.Borges <willianodhara@gmail.com>
2023-09-11 16:26:36 -0300

    Commit Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,27 +39,29 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  var numeroGerado = 0;
+
+  int _numeroAleatorio() {
+    Random numeroAleatorio = Random();
+    return numeroAleatorio.nextInt(1000);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("Meu App"),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add_alarm_outlined),
+          onPressed: () {
+            setState(() {
+              numeroGerado = _numeroAleatorio();
+            });
+          }),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Text(numeroGerado.toString()),
       ),
       /* floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
