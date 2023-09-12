d0c6a9ee5def114ad89ec5ff085e6bd4b1bb92d7
Willian.Borges <willianodhara@gmail.com>
2023-09-12 13:26:32 -0300

    Organizando Arquivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,7 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+import 'my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  var numeroGerado = 0;
-
-  int _numeroAleatorio() {
-    Random numeroAleatorio = Random();
-    return numeroAleatorio.nextInt(1000);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Meu App"),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add_alarm_outlined),
-          onPressed: () {
-            setState(() {
-              numeroGerado = _numeroAleatorio();
-            });
-          }),
-      body: Center(
-        child: Text(numeroGerado.toString()),
-      ),
-      /* floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), */ // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
 }
